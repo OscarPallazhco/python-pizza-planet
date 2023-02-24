c954d347d941914a664c21585c680e747f5e6c05
Oscar Pallazhco <oepallaz@gmail.com>
2023-02-23 21:46:11 -0500

    refactor: (PIZ3) Implement builder design pattern (#6)

--- app/controllers/order.py
+++ app/controllers/order.py
@@ -4,39 +4,22 @@ from ..common.utils import check_required_keys
 from ..repositories.managers import (IngredientManager, OrderManager,
                                      SizeManager, BeverageManager)
 from .base import BaseController
+from .concrete_order_builder import ConcreteOrderBuilder
 
 
 class OrderController(BaseController):
     manager = OrderManager
     __required_info = ('client_name', 'client_dni', 'client_address', 'client_phone', 'size_id')
 
-    @staticmethod
-    def calculate_order_price(size_price: float, ingredients: list, beverages: list):
-        ingredients_total = sum(ingredient.price for ingredient in ingredients)
-        beverages_total = sum(beverage.price for beverage in beverages)
-        price = size_price + ingredients_total + beverages_total
-        return round(price, 2)
 
     @classmethod
     def create(cls, order: dict):
-        current_order = order.copy()
-        if not check_required_keys(cls.__required_info, current_order):
-            return None, 'Invalid order payload'
-
-        size_id = current_order.get('size_id')
-        size = SizeManager.get_by_id(size_id)
-
-        if not size:
-            return None, 'Invalid size for Order'
-
-        ingredient_ids = current_order.pop('ingredients', [])
-        beverage_ids = current_order.pop('beverages', [])
-
         try:
-            ingredients = IngredientManager.get_by_id_list(ingredient_ids)
-            beverages = BeverageManager.get_by_id_list(beverage_ids)
-            price = cls.calculate_order_price(size.get('price'), ingredients, beverages)
-            order_with_price = {**current_order, 'total_price': price}
-            return cls.manager.create(order_with_price, ingredients, beverages), None
+            builder = ConcreteOrderBuilder()
+            builder.set_data(order.copy())
+            builder.check_required_info(cls.__required_info)
+            builder.check_size()
+            builder.calculate_order_price()
+            return builder.create()
         except (SQLAlchemyError, RuntimeError) as ex:
             return None, str(ex)
